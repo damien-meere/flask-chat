83e8cd24015503ffcb439da5a4450af7f05fee42
Damien Meere <damien.meere@bt.com>
2020-01-25 15:01:56 +0000

    gathering message content from text box (refresh issue to fix)

--- run.py
+++ run.py
@@ -30,9 +30,16 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/<username>")
+@app.route("/<username>", methods=["GET", "POST"])
 def user(username):
     """Display chat messages"""
+
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_messages(username, message)
+        return redirect(session["username"])
+
     #return "<h1>Welcome, {0}</h1>{1}".format(username, messages)
     return render_template("chat.html", username = username, chat_messages = messages)
 
